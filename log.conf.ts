796e5dc7a8a4e77cb817d35b2ce02a2730fcf609
Alina Kryuchkova <zuzikevich90@mail.ru>
2023-08-19 18:33:26 -0300

    delete report

---
+++ log.conf.ts
@@ -0,0 +1,12 @@
+import * as log4js from "log4js";
+log4js.configure({
+    appenders: { deb: { type: "file", filename: "deb.log" } },
+    categories: { default: { appenders: ["deb"], level: "error" } },
+});
+
+const logger = log4js.getLogger();
+logger.level = "debug";
+
+export {
+    logger
+}

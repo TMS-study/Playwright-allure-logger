698e5f77a8a1a110d30b2e95ec199be061eaf774
Alina Kryuchkova <zuzikevich90@mail.ru>
2023-08-19 19:59:06 -0300

    report

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ const config: PlaywrightTestConfig = {
     workers: 3,
     fullyParallel: true,
     reporter: [
-        //['html'],
+        ['html'],
         [
             "allure-playwright",
             {
